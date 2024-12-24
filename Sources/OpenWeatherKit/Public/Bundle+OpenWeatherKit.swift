3d5e8e737f7418db3572e3cf0d46dbc42d2f9cd7
Cameron Deardorff <cam@itsme.cam>
2024-12-23 17:21:47 -0700

    adding bundle extension for consumers to localize strings

---
+++ Sources/OpenWeatherKit/Public/Bundle+OpenWeatherKit.swift
@@ -0,0 +1,12 @@
+//
+//  Bundle+OpenWeatherKit.swift
+// 
+//
+//  Created by Cameron Deardorff on 12/23/24.
+//
+
+import Foundation
+
+public extension Bundle {
+    public static var OpenWeatherKit: Bundle { .module }
+}
